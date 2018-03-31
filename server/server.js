e0e82b8a16ac607cb7db692055e88c3616dee3f4
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-03-31 23:17:24 +0200

    server corr

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const jason = require('./jason.json')
 const app = express()
 
+app.use((request, response, next) => {
+	response.header('Access-Control-Allow-Origin', '*')
+	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	next()
+})
+
 app.get('/', (request, response) => {
 	response.send("Bienvenue sur notre site trop cool. Y'aura un formulaire d'inscription ici plus tard")
 })
