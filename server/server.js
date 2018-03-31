8c12f26533b4d0197dd18b4b71f064566fdf9a7c
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-03-31 22:55:47 +0200

    oops

--- server/server.js
+++ server/server.js
@@ -2,12 +2,6 @@ const express = require('express')
 const jason = require('./jason.json')
 const app = express()
 
-app.use((request, response, next) => {
-	response.header('Access-Control-Allow-Origin', '*')
-	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-	next()
-})
-
 app.get('/', (request, response) => {
 	response.send("Bienvenue sur notre site trop cool. Y'aura un formulaire d'inscription ici plus tard")
 })
@@ -20,4 +14,4 @@ app.get('/wildbook', (request, response) => {
 	response.json(jason)
 })
 
-app.listen(9000, () => console.log("j'écoute sur le port 9000"))
+app.listen(9000, () => console.log("Port 9000"))
