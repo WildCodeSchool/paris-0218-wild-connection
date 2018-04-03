6d92122ecb93e4698c9642aa1042e1c48a2cf25c
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-04-03 23:25:25 +0200

    filter on... dernier commit un peu degueu, promis

--- client/js/extractJson.js
+++ client/js/extractJson.js
@@ -1,22 +1,20 @@
-fetch('http://localhost:9000/wildbook')
-.then(response => response.json())
-.then(jason => {
-	console.log(jason)
-	let i = 0
-	while (jason[i]){
-		document.getElementById('info').innerHTML += 
-		`<div>
-		<div class="profile">
-		<div class="profilePic"><img src="http://via.placeholder.com/120x150"></div>
-		<div class="name">
-		<div><p>${jason[i].lastName}</p></div>
-		<div><p>${jason[i].firstName}</p></div>
-		<div><p>${jason[i].campus}</p></div>
-		<div><p>${jason[i].promo}</p></div>
-		</div>
-		</div>
-		<div>
-		`
-		i++
-	}
-})
+let i = 0
+
+
+while (jason[i]){
+	document.getElementById('info').innerHTML += 
+	`<div>
+	<div class="profile">
+	<div class="round"><img class="profilePic" src="../css/img/deer.png"></div>
+	<div class="name">
+	<div><p>${jason[i].lastName}</p></div>
+	<div><p>${jason[i].firstName}</p></div>
+	<div><p>${jason[i].campus}</p></div>
+	<div><p>${jason[i].promo}</p></div>
+	</div>
+	</div>
+	<div>
+	`
+	i++
+}
+
