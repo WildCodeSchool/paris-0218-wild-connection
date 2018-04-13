3805da9513553c4c0239e8cc05df14b237bef5cc
Alex Bui <alxbui14@gmail.com>
2018-04-13 16:44:23 +0200

    début correction

--- client/js/modal.js
+++ client/js/modal.js
@@ -1,29 +1,24 @@
 // Récupération de la div contenant l'ensemble du modal formulaire
 const modal = document.getElementById('simpleModal')
-// Récupération du bouton pour afficher le modal 
+// Récupération du bouton pour afficher le modal
 const modalBtn = document.getElementById('modalBtn')
-// Récupération du bouton pour fermer la fenêtre modal 
+// Récupération du bouton pour fermer la fenêtre modal
 const closeBtn = document.getElementsByClassName('closeBtn')[0]
 
+// Function pour afficher le modal :
+// on affiche la div modal (class="modal"), définie dans le css comme display 'none'
+// grâce au JS qui va changer le style 'none' à 'block' avec l'addEventListenir(click, openModal)
+const openModal = () => modal.style.display = 'block'
 
-// Function pour afficher le modal : 
-// on affiche la div modal (class="modal"), définie dans le css comme display 'none' 
-// grâce au JS qui va changer le style 'none' à 'block' avec l'addEventListenir(click, openModal) 
-const openModal = () => {
-	modal.style.display = 'block'
-}
-
-const closeModal = () => {
-	modal.style.display = 'none' 
-}
+const closeModal = () => modal.style.display = 'none'
 
 const clickOutside = (e) => {
 	if(e.target == modal){
-		modal.style.display = 'none' 
+		modal.style.display = 'none'
 	}
 }
 
-// Activer l'affichage du modal en display 'block' suite à l'event 'click'  
+// Activer l'affichage du modal en display 'block' suite à l'event 'click'
 modalBtn.addEventListener('click', openModal)
 
 // Fermer la fenêtre du modal en passant du display 'block' au display 'none' suite à l'event click sur la croix : <span class="closeBtn">&times;</span>
