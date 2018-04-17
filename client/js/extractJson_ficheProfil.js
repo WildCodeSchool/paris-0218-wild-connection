c9186f8a8610299c9b257f0198158b47f0ec1010
Alex Bui <alxbui14@gmail.com>
2018-04-17 10:45:18 +0200

    modifs gist mehdi

--- client/js/extractJson_ficheProfil.js
+++ client/js/extractJson_ficheProfil.js
@@ -1,83 +1,75 @@
-fetch('http://localhost:9000/wildbook')
-.then(response => jason = response.json())
-.then(jason =>{
+window.fetch('http://localhost:9000/wildbook')
+  .then(response => response.json())
+  .then(jason => {
+    /* injection des profiles en arrivant sur la pages */
+    document.getElementById('info').innerHTML = ''
 
-	/* injection des profiles en arrivant sur la pages */
-	document.getElementById('info').innerHTML = ''
+    jason.forEach((jason) => {
+      document.getElementById('info').innerHTML +=
+      `<div>
+      <div class="profile">
+      <div class="round"><img class="profilePic" src="../css/img/deer.png"></div>
+      <div class="name">
+      <div><p>${jason.lastName}</p></div>
+      <div><p>${jason.firstName}</p></div>
+      <div><p>${jason.campus}</p></div>
+      <div><p>${jason.promo}</p></div>
+      </div>
+      </div>
+      <div>`
+    })
+    /* voila !! tout les profiles sont affiché */
 
-	jason.forEach((jason) => {
-		document.getElementById('info').innerHTML += 
-		`<div>
-		<div class="profile">
-		<div class="round"><img class="profilePic" src="../css/img/deer.png"></div>
-		<div class="name">
-		<div><p>${jason.lastName}</p></div>
-		<div><p>${jason.firstName}</p></div>
-		<div><p>${jason.campus}</p></div>
-		<div><p>${jason.promo}</p></div>
-		</div>
-		</div>
-		<div>`
-	})
-	/* voila !! tout les profiles sont affiché */
+    /* injection des profiles après selection de filtre */
+    document.getElementById('value').addEventListener('click', () => {
+      /* ici je recupere les données du formulaire */
+      window.event.preventDefault()
+      let campus = document.getElementById('campus').value
+      let promo = document.getElementById('promo').value
+      let searchBar = document.getElementById('searchBar').value
+      let filterjason = jason
 
+      /* j'applique un filter sur le tableau jason avec les données du formulaire */
+      filterjason = jason.filter(profile => {
+        if ((profile.promo === promo || promo === 'Promo') && (profile.campus === campus || campus === 'Campus')) {
+          return true
+        }
+        return false
+      })
 
-	/* injection des profiles après selection de filtre */
-	document.getElementById('value').addEventListener('click', () => {
-		/* ici je recupere les données du formulaire */
-		event.preventDefault()
-		let campus = document.getElementById('campus').value
-		let promo = document.getElementById('promo').value
-		let techno = document.getElementById('techno').value
-		let searchBar = document.getElementById('searchBar').value
-		let filterjason = jason
-		let filterjasonbis
+      if (searchBar !== []) {
+        searchBar = searchBar.split(' ')
+        let i = 0
+        while (searchBar[i]) {
+          filterjason = filterjason.filter(profile => {
+            if (profile.promo === searchBar[i] || profile.campus === searchBar[i] || profile.firstName === searchBar[i] ||
+            profile.lastName === searchBar[i]) {
+              return true
+            }
+            return false
+          })
+          i++
+        }
+      }
 
-		/* j'applique un filter sur le tableau jason avec les données du formulaire */
+      /* j'efface les profiles affiché au chargement de la page */
+      document.getElementById('info').innerHTML = ''
 
-		filterjason = jason.filter(profile => {
-			if ((profile.promo === promo || promo === "Promo") && (profile.campus === campus || campus === 'Campus'))
-				return true
-			return false
-		})
-		
-		if (searchBar != []){
-			searchBar = searchBar.split(' ')
-			console.log(searchBar)
-			let i = 0
-			while (searchBar[i]) {
-				filterjason = filterjason.filter(profile => {
-					if (profile.promo === searchBar[i] 
-						|| profile.campus === searchBar[i] 
-						|| profile.firstName === searchBar[i] 
-						|| profile.lastName === searchBar[i])
-						return true
-					return false
-				})
-				i++
-			}
-			console.log(filterjason)
-		}
-
-		/* j'efface les profiles affiché au chargement de la page */
-		document.getElementById('info').innerHTML = ''
-
-		/* j'injecte le tableau json filtré */
-		filterjason.forEach(filterjason => {
-			document.getElementById('info').innerHTML += 
-			`<div>
-			<div class="profile">
-			<div class="round"><img class="profilePic" src="../css/img/deer.png"></div>
-			<div class="name">
-			<div><p>${filterjason.lastName}</p></div>
-			<div><p>${filterjason.firstName}</p></div>
-			<div><p>${filterjason.campus}</p></div>
-			<div><p>${filterjason.promo}</p></div>
-			</div>
-			</div>
-			<div>`
-		})
-
-		filterjason = jason
-	})
-			})
+      /* j'injecte le tableau json filtré */
+      filterjason.forEach(filterjason => {
+        document.getElementById('info').innerHTML +=
+        `<div>
+        <div class="profile">
+        <div class="round"><img class="profilePic" src="../css/img/deer.png"></div>
+        <div class="name">
+        <div><p>${filterjason.lastName}</p></div>
+        <div><p>${filterjason.firstName}</p></div>
+        <div><p>${filterjason.campus}</p></div>
+        <div><p>${filterjason.promo}</p></div>
+        </div>
+        </div>
+        <div>`
+      })
+      filterjason = jason
+    })
+  })
