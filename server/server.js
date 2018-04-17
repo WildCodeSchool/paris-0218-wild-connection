af2b0f77116fc047ea3960fb7d8973611891c5ba
Alex Bui <alxbui14@gmail.com>
2018-04-17 10:50:14 +0200

    server lint

--- server/server.js
+++ server/server.js
@@ -4,21 +4,21 @@ const wildjob = require('./wildjob-mock.json')
 const app = express()
 
 app.use((request, response, next) => {
-	response.header('Access-Control-Allow-Origin', '*')
-	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-	next()
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
 
 app.get('/', (request, response) => {
-	response.send("Bienvenue sur notre site trop cool. Y'aura un formulaire d'inscription ici plus tard")
+  response.send("Bienvenue sur notre site trop cool. Y'aura un formulaire d'inscription ici plus tard")
 })
 
 app.get('/wildjob', (request, response) => {
-	response.json(wildjob)
+  response.json(wildjob)
 })
 
 app.get('/wildbook', (request, response) => {
-	response.json(jason)
+  response.json(jason)
 })
 
-app.listen(9000, () => console.log("Port 9000"))
+app.listen(9000, () => console.log('Port 9000'))
