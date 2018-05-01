66dd4d11f6e9b633801e484e2a629fd872b8d858
Franck David <oxflow42@gmail.com>
2018-05-02 01:52:25 +0200

    css book

--- client/js/components/show-user.js
+++ client/js/components/show-user.js
@@ -1,10 +1,11 @@
 export const showUser = fullUser => `
 
-  <div class='${fullUser.color}' id='${fullUser.id}'>
-    <div class='round'><img class='profile-pic' src='${fullUser.image}'></div>
-    <div class='name'>
-      <div><p>${fullUser.firstName}</p></div>
-      <div><p>${fullUser.lastName}</p></div>
+  <div class='${fullUser.color} opacity' id='${fullUser.id}'>
+    <div class="header">
+    <div class='centered round'><img class='profile-pic center-pic' src='${fullUser.image}'></div>
+    </div>
+    <div class='name font-modal'>
+      <div><p>${fullUser.firstName} ${fullUser.lastName}</p></div>
       <div><p>${fullUser.campus}</p></div>
       <div><p>${fullUser.promo}</p></div>
     </div>
