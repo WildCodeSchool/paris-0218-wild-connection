0f9f8fc10449b8a5629d114a579e61d399baaf85
ylatorre <yvanlatorre@hotmail.fr>
2018-05-02 11:45:55 +0200

    scroll

---
+++ client/html/HOME/js/test.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // au clic sur un lien
+    $('a').on('click', function(evt){
+       // bloquer le comportement par défaut: on ne rechargera pas la page
+       evt.preventDefault();
+       // enregistre la valeur de l'attribut  href dans la variable target
+	var target = $(this).attr('href');
+       /* le sélecteur $(html, body) permet de corriger un bug sur chrome
+       et safari (webkit) */
+	$('html, body')
+       // on arrête toutes les animations en cours
+       .stop()
+       /* on fait maintenant l'animation vers le haut (scrollTop) vers
+        notre ancre target */
+       .animate({scrollTop: $(target).offset().top}, 1000 );
+    });
+});
