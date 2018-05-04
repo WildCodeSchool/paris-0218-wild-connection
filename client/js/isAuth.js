870e80f6591af43a6b02b04a932b0488dcddda6a
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-05-04 13:55:35 +0200

    c chiant

--- client/js/isAuth.js
+++
@@ -1,4 +0,0 @@
-	fetch('http://localhost:3456/', {'credentials': 'include'})
-	.then (res => res.json)
-	.then (res => if (res !== 'Ok') window.location.replace('http://localhost:5000/html/login.html'))
-})
